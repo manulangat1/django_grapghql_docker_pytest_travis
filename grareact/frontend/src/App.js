0f1704da456b51aecbc67c56241d4359de6a94a0
manulangat <emmanuelthedeveloper@gmail.com>
2020-07-31 15:29:29 +0300

    ft:installing_grapghql_dependancies:frontend

--- grareact/frontend/src/App.js
+++ grareact/frontend/src/App.js
@@ -2,6 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+
+// 2
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+
+// 3
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
 function App() {
   return (
     <div className="App">
